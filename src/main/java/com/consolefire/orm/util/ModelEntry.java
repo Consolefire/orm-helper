4b47816bf124ec744a4ed822006470f48f2cf266
Sabuj Das <sabuj.das@flipkart.com>
2016-04-14 11:04:49 +0530

    adding first version

---
+++ src/main/java/com/consolefire/orm/util/ModelEntry.java
@@ -0,0 +1,19 @@
+package com.consolefire.orm.util;
+
+import lombok.Getter;
+
+/**
+ * Created by sabuj.das on 13/04/16.
+ * @author sabuj.das
+ */
+@Getter
+public final class ModelEntry<V> {
+    private final String fieldName;
+    private final V value;
+
+    public ModelEntry(String fieldName, V value) {
+        this.fieldName = fieldName;
+        this.value = value;
+    }
+
+}
